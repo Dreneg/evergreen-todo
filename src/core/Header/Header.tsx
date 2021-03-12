621c6829037ccedb9a6cbf1660d6edc164808166
Dreneg <dawidsandor@gmail.com>
2021-03-12 21:35:58 +0100

    Add icon to the header component

--- src/core/Header/Header.tsx
+++ src/core/Header/Header.tsx
@@ -3,8 +3,9 @@ import './Header.css';
 
 function Header(): JSX.Element {
   return (
-    <header className="header">
-      <span className="mx-sm-2 mx-lg-4 h3">EverGreen Todo</span>
+    <header className="header d-flex align-content-center flex-wrap">
+      <span className="icon bi bi-kanban mx-2 mx-lg-4 d-"></span>
+      <span>EverGreen Todo</span>
     </header>
   );
 }
